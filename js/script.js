cd64789ce59782abe5b545a7d25c79d785fb4395
Vladimir Kozinsky <kozinskytxc@gmail.com>
2021-10-22 16:00:02 +0400

    feat: add date update

--- js/script.js
+++ js/script.js
@@ -5,6 +5,21 @@ const showTime = () => {
     const currentTime = date.toLocaleTimeString()
     time.textContent = currentTime;
     setTimeout(showTime, 1000);
+    const option = {hour: 'numeric', minute:'numeric', second: 'numeric' }
+    let midNight = date.toLocaleTimeString('en-US', option)
+    if ('12:00:00 AM' === midNight ) {
+        showDate();
+    }
 }
 
-showTime()
+showTime();
+
+const showDate = () => {
+    const dateSel = document.querySelector(".date");
+    const date = new Date();
+    const options = { weekday: 'long', month: 'long', day: 'numeric', timeZone: 'UTC' };
+    const currentDate = date.toLocaleDateString('en-US', options);
+    dateSel.textContent = currentDate;
+}
+
+showDate();
